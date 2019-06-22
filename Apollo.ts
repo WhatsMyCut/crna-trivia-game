634925b0050fa4ed957cc025137b747fb51681f5
Mike Taylor <mike@whatsmycut.com>
2019-06-21 18:00:37 -0700

    Add Apollo libs for orientation handling + Fonts

---
+++ Apollo.ts
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink, split } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { RetrieveData } from './store/AsyncStore';
+
+const httpLink = new HttpLink({
+  uri: `https://eg-production.herokuapp.com/`
+  // uri: `http://localhost:4000`
+});
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(
+        'multiple errors',
+        JSON.stringify({ message, locations, path })
+      );
+      return message;
+    });
+  }
+
+  if (networkError) {
+    console.log(`Network error: ${networkError}`);
+  }
+});
+
+const authLink = setContext(async (_, context) => {
+  const headers = { ...context.headers };
+  const token = await RetrieveData('EARTH_GUARDIANS_TOKEN'); // retrieve from asyncstorage
+  if (token) {
+    headers.authorization = `Bearer ${token}`;
+  }
+
+  return {
+    ...context,
+    headers,
+  };
+});
+
+// For info on how to use navigation service
+// https://reactnavigation.org/docs/en/navigating-without-navigation-prop.html#docsNav
+
+export default client = new ApolloClient({
+  ssrForceFetchDelay: 100,
+  link: ApolloLink.from([authLink, errorLink, httpLink]),
+  cache: new InMemoryCache(),
+});
+
