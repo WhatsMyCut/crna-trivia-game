b1128a453befe3810d88d5e8f913d8a11661231a
Mike Taylor <mike@whatsmycut.com>
2019-06-23 12:41:58 -0700

    Load the results

--- components/game-nav/index.tsx
+++ components/game-nav/index.tsx
@@ -12,18 +12,18 @@ const styles = { ...Styles, ...{
 
 export default class GamePage extends Component<IProps, IState> {
 
-render () {
-  return (
+  render () {
+    return (
 
-    <View style={[styles.topNav]}>
-      <TouchableOpacity
-        onPress={() => this.props.navigation.navigate('AuthLoading', {})}>
-        <Ionicons name="ios-arrow-round-back" size={42} color="#ccc" />
-      </TouchableOpacity>
-      <View style={[styles.headerContainer]}>
-        <Text style={[styles.headerText]}>Trivia Challenge</Text>
+      <View style={[styles.topNav]}>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate('AuthLoading', {})}>
+          <Ionicons name="ios-arrow-round-back" refs={{}} size={42} color="#ccc" />
+        </TouchableOpacity>
+        <View style={[styles.headerContainer]}>
+          <Text style={[styles.headerText]}>Trivia Challenge</Text>
+        </View>
       </View>
-    </View>
 
     );
   }
--- components/results-page/index.tsx
+++ components/results-page/index.tsx
@@ -4,24 +4,62 @@ import {
   SafeAreaView,
   View,
   TouchableOpacity,
-  Text
+  Text,
+  ActivityIndicator
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { RetrieveData } from '../../store/AsyncStore';
 const styles = { ...Styles, ...{
 
 }};
 
-export default class ResultsPage extends Component<IProps, IState> {
+export interface IProps {};
+export interface IState {
+  questions?: any;
+  isLoaded?: boolean;
+};
 
-render () {
-  return (
-    <SafeAreaView style={[styles.container, styles.coverScreen, styles.centerAll]}>
-      <View style={[styles.topNav]}>
-        <View style={[styles.headerContainer]}>
-          <Text style={[styles.headerText]}>Trivia Challenge</Text>
+export interface IQuestion {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: [string];
+  question: string;
+  type: string;
+  given_answer?: string;
+}
+
+
+export default class ResultsPage extends Component<IProps, IState> {
+  constructor(props: IProps, state: IState) {
+    super(props);
+    this.state = {...state, ...{
+      isLoaded: false,
+    }}
+  }
+
+  componentWillMount() {
+    RetrieveData('questions')
+    .then(questions =>this.setState({questions, isLoaded: true,}));
+  }
+
+  render () {
+    const { isLoaded } = this.state;
+    if (!isLoaded) {
+      return <ActivityIndicator size="large" color="#0000ff" />;
+    }
+    const { questions } = this.state;
+    console.log('RESULTS: ', questions);
+    return (
+      <SafeAreaView style={[styles.container, styles.coverScreen, styles.centerAll]}>
+        <View style={[styles.topNav]}>
+          <View style={[styles.headerContainer]}>
+            <Text style={[styles.headerText]}>
+              {questions.length}
+            </Text>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
 
     );
   }
